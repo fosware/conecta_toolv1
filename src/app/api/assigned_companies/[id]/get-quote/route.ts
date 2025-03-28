cb0e256667327a7dfc08a03f6ce0b6c5890f9875
fosware <fosahadal@protonmail.com>
2025-03-27 23:08:26 -0600

    fix import client prisma quote

--- src/app/api/assigned_companies/[id]/get-quote/route.ts
+++ src/app/api/assigned_companies/[id]/get-quote/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import { getUserFromToken } from "@/lib/get-user-from-token";
-
-const prisma = new PrismaClient();
 
 export async function GET(
   request: NextRequest,
